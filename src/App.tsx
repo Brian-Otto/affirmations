c908b1a7a6a726eeb07b04b58b2828b9d841b536
Brian <184952956+Brian-Otto@users.noreply.github.com>
2026-02-27 23:02:45 +0100

    Add test code

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import AffirmationIcon from "./components/AffirmationIcon";
+
 function App() {
-  return <></>;
+  return <div className="h-screen bg-ctp-base flex flex-col justify-center items-center">
+    <AffirmationIcon className="text-ctp-text w-20 h-20" />
+  <div className="text-ctp-text text-2xl font-IndieFlower">Ich bin ein Test und diene dem Testen von Dingen.</div>
+  </div>;
 }
 
 export default App;
