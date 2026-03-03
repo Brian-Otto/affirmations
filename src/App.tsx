113f930bfbe950bfab78e430f08c40a17c897955
Brian <184952956+Brian-Otto@users.noreply.github.com>
2026-03-03 23:06:03 +0100

    Add new component and update styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import AffirmationIcon from "./components/AffirmationIcon";
+import AffirmationText from "./components/AffirmationText";
 
 function App() {
-  return <div className="h-screen bg-ctp-base flex flex-col justify-center items-center">
-    <AffirmationIcon className="text-ctp-text w-20 h-20" />
-  <div className="text-ctp-text text-2xl font-IndieFlower">Ich bin ein Test und diene dem Testen von Dingen.</div>
-  </div>;
+  return (
+    <div className="h-screen bg-ctp-base flex justify-center items-center">
+      <div className="max-w-sm md:max-w-md lg:max-w-lg p-6 flex flex-col gap-4 items-center text-center">
+      <AffirmationIcon className="text-ctp-text w-20 h-20" />
+      <AffirmationText className="text-ctp-text text-2xl font-IndieFlower" />
+      </div>
+    </div>
+  );
 }
 
 export default App;
