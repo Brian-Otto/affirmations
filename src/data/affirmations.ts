d334780c7f1c848d5e6c4989b1ace6656cf98382
Brian <184952956+Brian-Otto@users.noreply.github.com>
2026-03-03 23:04:40 +0100

    Add affirmations

---
+++ src/data/affirmations.ts
@@ -0,0 +1,12 @@
+const affirmations = [
+  {
+    id: 0,
+    text: "Ich darf mir Zeit für mich nehmen.",
+  },
+  {
+    id: 1,
+    text: "Ich erlaube mir, Fehler zu machen und aus ihnen zu lernen.",
+  },
+];
+
+export default affirmations;
