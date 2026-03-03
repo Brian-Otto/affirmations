39203a4718775402a9e88aa94c74788bb121df8e
Brian <184952956+Brian-Otto@users.noreply.github.com>
2026-03-03 23:05:46 +0100

    Add component
    Displays text from affirmations, changes daily and handles resets, list gets saved to rotate through without repeats

---
+++ src/components/AffirmationText.tsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+import affirmations from "../data/affirmations";
+
+const LOCAL_STORAGE_DATE_KEY = "affirmationDate";
+const LOCAL_STORAGE_TODAY_KEY = "affirmationToday";
+const LOCAL_STORAGE_LIST_KEY = "affirmationsList";
+
+function AffirmationText({ className = "" }) {
+  const [id, setId] = useState<number | null>(null);
+
+  useEffect(() => {
+    const todayString = new Date().toDateString();
+    const storedDate = localStorage.getItem(LOCAL_STORAGE_DATE_KEY);
+
+    if (storedDate !== todayString) {
+      localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify([]));
+      localStorage.setItem(LOCAL_STORAGE_TODAY_KEY, "");
+      localStorage.setItem(LOCAL_STORAGE_DATE_KEY, todayString);
+    }
+
+    const storedList = localStorage.getItem(LOCAL_STORAGE_LIST_KEY);
+
+    let shownAffirmationsArray: number[] = storedList
+      ? JSON.parse(storedList)
+      : [];
+
+    let unshownAffirmations = affirmations.filter(
+      (aff) => !shownAffirmationsArray.includes(aff.id),
+    );
+
+    if (unshownAffirmations.length === 0) {
+      shownAffirmationsArray = [];
+      localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify([]));
+      unshownAffirmations = [...affirmations];
+    }
+
+    const storedIdString = localStorage.getItem(LOCAL_STORAGE_TODAY_KEY);
+    let storedId = storedIdString ? Number(storedIdString) : null;
+
+    if (storedId === null || !affirmations.some((aff) => aff.id === storedId)) {
+      const randomAffirmation =
+        unshownAffirmations[
+          Math.floor(Math.random() * unshownAffirmations.length)
+        ];
+      storedId = randomAffirmation.id;
+      localStorage.setItem(LOCAL_STORAGE_TODAY_KEY, String(storedId));
+    }
+
+    if (!shownAffirmationsArray.includes(storedId))
+      shownAffirmationsArray.push(storedId);
+
+    localStorage.setItem(
+      LOCAL_STORAGE_LIST_KEY,
+      JSON.stringify(shownAffirmationsArray),
+    );
+
+    setId(storedId);
+  }, []);
+
+  const affirmation = affirmations.find((aff) => aff.id === id);
+
+  if (!affirmation) return null;
+
+  return <span className={className}>{affirmation.text}</span>;
+}
+
+export default AffirmationText;
