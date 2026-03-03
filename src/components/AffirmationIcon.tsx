520fcafbdf5d902e3d40d71c0ccbf4d7f3c26c4a
Brian <184952956+Brian-Otto@users.noreply.github.com>
2026-03-03 23:04:27 +0100

    Update to reset daily

--- src/components/AffirmationIcon.tsx
+++ src/components/AffirmationIcon.tsx
@@ -3,23 +3,33 @@ import Cat from "../assets/images/cat.svg?react";
 import CatCurled from "../assets/images/cat-curled.svg?react";
 
 const icons = [Cat, CatCurled];
-const LOCAL_STORAGE_KEY = "todaysIcon";
+const LOCAL_STORAGE_DATE_KEY = "affirmationDate";
+const LOCAL_STORAGE_TODAY_KEY = "iconToday";
 
 function AffirmationIcon({ className = "" }) {
-  const [index, setIndex] = useState<number|null>(null);
+  const [index, setIndex] = useState<number | null>(null);
 
   useEffect(() => {
-    const storedIndexString = localStorage.getItem(LOCAL_STORAGE_KEY);
+    const todayString = new Date().toDateString();
+    const storedDate = localStorage.getItem(LOCAL_STORAGE_DATE_KEY);
+
+    if (storedDate !== todayString) {
+      localStorage.setItem(LOCAL_STORAGE_TODAY_KEY, "");
+      localStorage.setItem(LOCAL_STORAGE_DATE_KEY, todayString);
+    }
+
+    const storedIndexString = localStorage.getItem(LOCAL_STORAGE_TODAY_KEY);
     let storedIndex = storedIndexString ? Number(storedIndexString) : null;
 
     if (storedIndex === null) {
       storedIndex = Math.floor(Math.random() * icons.length);
-      localStorage.setItem(LOCAL_STORAGE_KEY, storedIndex.toString());
-    } else {
-      storedIndex = Number(storedIndex);
+      localStorage.setItem(
+        LOCAL_STORAGE_TODAY_KEY,
+        String(storedIndex),
+      );
     }
 
-    setIndex(storedIndex)
+    setIndex(storedIndex);
   }, []);
 
   if (index === null) return null;
