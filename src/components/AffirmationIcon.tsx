659840e39ac7e73474f8c51cb3c1032cbde6371e
Brian <184952956+Brian-Otto@users.noreply.github.com>
2026-03-06 22:43:13 +0100

    Update daily switch logic to not select yesterdays icon
    Now it's easier testable and also more user-friendly

--- src/components/AffirmationIcon.tsx
+++ src/components/AffirmationIcon.tsx
@@ -4,30 +4,40 @@ const LOCAL_STORAGE_DATE_KEY = "affirmationDate";
 const LOCAL_STORAGE_TODAY_KEY = "iconToday";
 
 /**
- * Returns the icon index for today from localStorage. 
+ * Returns the icon index for today from localStorage.
  * New random index is stored in localStorage, when the day changes.
- * 
+ *
  * @param iconCount amount of icons
  * @returns selected index for today
  */
 export function getIconTodayIndex(iconCount: number): number {
   const todayString = new Date().toDateString();
   const storedDate = localStorage.getItem(LOCAL_STORAGE_DATE_KEY);
+  const storedIndex = localStorage.getItem(LOCAL_STORAGE_TODAY_KEY);
 
-  if (storedDate !== todayString) {
-    localStorage.setItem(LOCAL_STORAGE_DATE_KEY, todayString);
-    localStorage.removeItem(LOCAL_STORAGE_TODAY_KEY);
+  if (storedDate === todayString && storedIndex !== null) {
+    return Number(storedIndex);
   }
 
-  const stored = localStorage.getItem(LOCAL_STORAGE_TODAY_KEY);
-  if (stored !== null) return Number(stored);
+  localStorage.setItem(LOCAL_STORAGE_DATE_KEY, todayString);
 
-  const index = Math.floor(Math.random() * iconCount);
+  let index;
+  do {
+    index = Math.floor(Math.random() * iconCount);
+  } while (iconCount > 1 && String(index) === storedIndex);
+
   localStorage.setItem(LOCAL_STORAGE_TODAY_KEY, String(index));
   return index;
 }
 
-const icons = [Cat, CatCurled];
+// [CatComponent, CatCurledComponent, ...]
+const icons = Object.values(
+  import.meta.glob("../assets/images/doodles/*.svg", {
+    eager: true,
+    query: "?react",
+    import: "default",
+  }),
+) as React.FunctionComponent<React.SVGProps<SVGSVGElement>>[];
 
 function AffirmationIcon({ className = "" }) {
   const [index, setIndex] = useState<number | null>(null);
