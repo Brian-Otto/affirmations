42c0ee71a3a069dd46cbd20cbc34fb7853cc0fc1
Brian <184952956+Brian-Otto@users.noreply.github.com>
2026-03-19 19:57:28 +0100

    Implement basic pwa plugin config
    PWA Minimal Requirements not yet fulfilled, service worker and caching seems to work

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import svgr from "vite-plugin-svgr";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    svgr(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: { enabled: true },
+      workbox: { globPatterns: ["**/*.{js,css,html,ico,png,svg,woff}"] },
+    }),
+  ],
   test: { environment: "jsdom" },
-  base: '/affirmations/'
+  base: "/affirmations/",
 });
